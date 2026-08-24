f85c3b8bef1c32dfcda8af31de7fbd1987f00417
Faraz Sarfaraz Agha <fagha4697@gmail.com>
2026-08-25 00:42:34 +0530

    commit

---
+++ palindromeNumber.js
@@ -0,0 +1,18 @@
+// Program for the palindrome number
+
+function palindromeNumber(num){
+let rem;
+let original=num;
+let rev=0;
+  while(num>0){
+    rem=num%10;
+ rev=rev*10 +rem;
+ num=Math.floor(num/10);
+  }
+if(rev===original){
+  return("Palindrome Number");
+}else{
+  return ("Non Palindrome number");
+}
+
+}console.log(palindromeNumber(121));
