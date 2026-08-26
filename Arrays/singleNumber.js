3af35dcc3723f4aaa8f8758e23b3f3102d9e055c
Faraz Sarfaraz Agha <fagha4697@gmail.com>
2026-08-27 01:29:55 +0530

    Single Number

--- Arrays/singleNumber.js
+++ Arrays/singleNumber.js
@@ -1,11 +1,21 @@
 function singleNumber(nums){
 let c=0;
-let j=0;
-for(let i=1;i<nums.length;i++){
-if(nums[j]===nums[i]){
-  j ++;
+
+for (let j = 0; j < nums.length; j++) {
+
+    let count=0;
+
+    for (let i = 0; i < nums.length; i++) {
+
+        if (nums[j] === nums[i]) {
+            count++;
+        }
+    }
+
+    if (count === 1) {
+        return nums[j];
+    }
 }
-return nums[j];
+
 }
-}
-console.log(singleNumber([1,2,3,2,3]));
+console.log(singleNumber([1,2,3,2,3,1,5]));
