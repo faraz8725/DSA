39fc9fdc75510070fd681193986173b292c30331
Faraz Sarfaraz Agha <fagha4697@gmail.com>
2026-09-05 00:15:17 +0530

    Leetcode number 28

--- String/firstOccurence.js
+++ String/firstOccurence.js
@@ -1,4 +1,29 @@
-function first(haystack,needle){
 
 
+
+function first(s1, s2) {
+
+  for(let i = 0; i < s1.length; i++) {
+
+    let found = true;
+
+    for(let j = 0; j < s2.length; j++) {
+
+      let char = s1.charAt(i + j);
+      let char2 = s2.charAt(j);
+
+      if(char !== char2) {
+        found = false;
+        break;
+      }
+    }
+
+    if(found) {
+      return i;
+    }
+  }
+
+  return -1;
 }
+
+console.log(first("farazlovewaliya", "waliya"));
